96f137bfec31cdb3d279b0d89104a5a7b900498e
ictxiangxin <ictxiangxin@gmail.com>
2017-03-23 23:04:38 +0800

    prepare for convolutional neural network.

--- paradox/__init__.py
+++ paradox/__init__.py
@@ -1,4 +1,5 @@
 from paradox.kernel import *
 import paradox.neural_network as nn
+import paradox.neural_network.convolutional_neural_network as cnn
 from paradox.visualization import VisualLoss
 import paradox.data_set as data
