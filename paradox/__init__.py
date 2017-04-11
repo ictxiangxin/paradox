3774574c30c01ebb15056c1c226051244b65ebfb
ictxiangxin <ictxiangxin@gmail.com>
2017-04-11 23:45:52 +0800

    remove import VisualLoss.

--- paradox/__init__.py
+++ paradox/__init__.py
@@ -1,6 +1,5 @@
 from paradox.kernel import *
 import paradox.neural_network as nn
 import paradox.neural_network.convolutional_neural_network as cnn
-from paradox.visualization import VisualLoss
 import paradox.data as data
 import paradox.utils as utils
