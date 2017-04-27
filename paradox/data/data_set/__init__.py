ba6bf43f884c017aff5b5794ad0b44b481fffdcb
lxm <l11x0m7@bupt.edu.cn>
2017-04-27 21:08:19 +0800

    add three data-2d producers

--- paradox/data/data_set/__init__.py
+++ paradox/data/data_set/__init__.py
@@ -1,2 +1,5 @@
 from paradox.data.data_set.data_2d import helical_data as helical_2d
 from paradox.data.data_set.data_2d import grid_data as grid_2d
+from paradox.data.data_set.data_2d import circle_data as circle_2d
+from paradox.data.data_set.data_2d import gaussian_data as gaussian_2d
+from paradox.data.data_set.data_2d import cross_data as cross_2d
--- paradox/data/data_set/data_2d.py
+++ paradox/data/data_set/data_2d.py
@@ -7,7 +7,8 @@ def helical_data(number: int,
                  max_radius: int=5,
                  max_theta: float=3*numpy.pi,
                  init_radius: float=0,
-                 init_theta: float=0):
+                 init_theta: float=0,
+                 center: list=[0, 0]):
     radius_step = max_radius / number
     theta_step = max_theta / number
     phase = 2 * numpy.pi / category
@@ -15,10 +16,11 @@ def helical_data(number: int,
     start_phase = [i * phase for i in range(category)]
     radius = init_radius
     theta = init_theta
+    x0, x1 = center
     for _ in range(number):
         for i in range(category):
-            data[i][0].append(radius * numpy.cos(theta + start_phase[i]) + numpy.random.normal(0, noise))
-            data[i][1].append(radius * numpy.sin(theta + start_phase[i]) + numpy.random.normal(0, noise))
+            data[i][0].append(x0 + radius * numpy.cos(theta + start_phase[i]) + numpy.random.normal(0, noise))
+            data[i][1].append(x1 + radius * numpy.sin(theta + start_phase[i]) + numpy.random.normal(0, noise))
         radius += radius_step
         theta += theta_step
     return data
@@ -41,4 +43,59 @@ def grid_data(number: int,
                 y = numpy.random.random() * raw_step
                 data[(i + j) % 2][0].append(x + column_step * j + numpy.random.normal(0, noise))
                 data[(i + j) % 2][1].append(y + raw_step * i + numpy.random.normal(0, noise))
+    return data
+
+
+def circle_data(number: int,
+                category: int=2,
+                center: list=[0, 0],
+                delta_radius: float=1.,
+                noise: float=0.
+                ):
+    data = [[[], []] for _ in range(category)]
+    radius = 0.
+    theta_step = 2. * numpy.pi / number
+    x0, x1 = center
+    for i in range(category):
+        radius += delta_radius
+        theta = 0.
+        for _ in range(number):
+            data[i][0].append(x0 + radius * numpy.cos(theta) + numpy.random.normal(0, noise))
+            data[i][1].append(x1 + radius * numpy.sin(theta) + numpy.random.normal(0, noise))
+            theta += theta_step
+    return data
+
+
+def gaussian_data(number: int,
+                  category: int=2,
+                  mean: list=None,
+                  cov: list=None):
+    data = []
+    mean = mean or [[i + 1, i + 1] for i in range(category)]
+    cov = cov or [numpy.eye(2) for _ in range(category)]
+    for i in range(category):
+        data.append(numpy.random.multivariate_normal(mean[i], cov[i], number).T)
+    return data
+
+
+def cross_data(number: int,
+               category: int=2,
+               center: list=[0, 0],
+               radius: float=2,
+               init_angle: float=0.,
+               noise: float=0.):
+    delta_angle = numpy.pi / category
+    data = [[[], []] for _ in range(category)]
+    x0, x1 = center
+    for i in range(category):
+        x_step = 2. * radius * numpy.cos(init_angle) / number
+        y_step = 2. * radius * numpy.sin(init_angle) / number
+        x_start = x0 + radius * numpy.cos(init_angle)
+        y_start = x1 + radius * numpy.sin(init_angle)
+        for _ in range(number):
+            data[i][0].append(x_start + numpy.random.normal(0, noise))
+            data[i][1].append(y_start + numpy.random.normal(0, noise))
+            x_start -= x_step
+            y_start -= y_step
+        init_angle += delta_angle
     return data
