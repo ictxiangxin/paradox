9b57298133cf7da54757359e1b9eb391c845306c
ictxiangxin <ictxiangxin@gmail.com>
2017-08-27 22:16:20 +0800

    bug fix: iteration speed.

--- paradox/neural_network/network.py
+++ paradox/neural_network/network.py
@@ -206,9 +206,9 @@ class Network:
                     if batch_size != 0:
                         self.engine.bind = {self.__input_symbol: data[i: min([i + batch_size, data_scale])],
                                             target_symbol: target[i: min([i + batch_size, data_scale])]}
-                    self.iteration += 1
                     self.run_plugin('begin_iteration')
                     self.__optimizer.minimize(self.engine)
+                    self.iteration += 1
                     self.run_plugin('end_iteration')
                 self.run_plugin('end_epoch')
         except KeyboardInterrupt:
